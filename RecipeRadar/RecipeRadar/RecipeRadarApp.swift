badbef8c24204bc6b2baba2ebe2481c449c462bc
Dominik Babić <domynick93@hotmail.com>
2024-03-02 13:28:16 +0100

    Adds SwiftUI app base to the workspace.

---
+++ RecipeRadar/RecipeRadar/RecipeRadarApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeRadarApp.swift
+//  RecipeRadar
+//
+//  Created by Dominik Babic on 02.03.2024..
+//
+
+import SwiftUI
+
+@main
+struct RecipeRadarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
