be6716ef28c67377949ff72ccf96c2def46d4aa4
Dominik Babić <domynick93@hotmail.com>
2024-03-03 14:56:07 +0100

    Adds logic for loading more data.

--- RecipeRadar/RecipeRadar/Dashboard/DashboardViewModel.swift
+++ RecipeRadar/RecipeRadar/Dashboard/DashboardViewModel.swift
@@ -22,7 +22,13 @@ final class DashboardViewModel: ObservableObject {
     @Published var text: String = ""
     @Published var data: [Recipe] = []
     
+    @Published var didChange = false
+    @Published var hasMore = true
+    
     @Published var error: Error?
+    
+    var pageStart = 0
+    var pageEnd = 1
     
     init(networkService: NetworkServiceProtocol) {
         self.networkService = networkService
@@ -34,23 +40,43 @@ final class DashboardViewModel: ObservableObject {
         $text
             .removeDuplicates()
             .sink { [weak self] _ in
-                self?.data = []
+                self?.didChange = true
+                self?.resetPage()
             }
             .store(in: &cancellables)
     }
-
+    
     // TODO: Method for network request
     @MainActor
     func fetchData() async {
         do {
-            let route = RecipeRoute(recipe: text, from: 0, to: 10)
+            guard hasMore else { return }
+            
+            let route = RecipeRoute(recipe: text, from: pageStart, to: pageEnd)
             let url = try route.apiURL()
             let request = URLRequest(url: url)
             
             let recipeList: RecipeList = try await networkService.request(request)
+            
             data = recipeList.hits.map { $0.recipe }
+            
+            let totalCount = recipeList.count
+            hasMore = recipeList.more
+            pageStart = pageEnd
+            
+            if pageEnd + 10 < totalCount {
+                pageEnd += 10
+            } else {
+                pageEnd = totalCount
+            }
         } catch let apiError {
             error = apiError
         }
+    }
+    
+    func resetPage() {
+        didChange = false
+        pageStart = 0
+        pageEnd = 10
     }
 }
